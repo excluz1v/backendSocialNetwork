1d67ca57dfb9dd965e0d7cfc2b17d440912d3892
excluz1v <bmd4work@gmail.com>
2020-03-24 23:32:29 +0500

    first commit

---
+++ functions/utility/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: "AIzaSyCIMHJsfGvrrr4S1sqSk2eMR5ZFEU8sUBY",
+    authDomain: "social-network-v2.firebaseapp.com",
+    databaseURL: "https://social-network-v2.firebaseio.com",
+    projectId: "social-network-v2",
+    storageBucket: "social-network-v2.appspot.com",
+    messagingSenderId: "1083062121099",
+    appId: "1:1083062121099:web:345890d6d39dff42341a8e",
+    measurementId: "G-QM3QP8C6QR"
+
+}
